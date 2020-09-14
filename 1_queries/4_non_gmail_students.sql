a85598c6134b0832c416a229c2e503e0aa2dd73a
meriem farsi <farsi.meriem60@gmail.com>
2020-09-14 21:33:53 +0000

    1_queries

---
+++ 1_queries/4_non_gmail_students.sql
@@ -0,0 +1,4 @@
+SELECT id, name, cohort_id
+FROM students
+WHERE phone IS NULL 
+AND email NOT LIKE '%@gmail.com'; 
---
+++ 1_queries/5_currently_enrolled_students.sql
@@ -0,0 +1,4 @@
+SELECT id, name, cohort_id
+FROM students
+WHERE end_date is null
+ORDER BY cohort_id;
