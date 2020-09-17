0989d2e6b60b35c20d9a6951766fe622d004d5d2
meriem farsi <farsi.meriem60@gmail.com>
2020-09-17 17:43:23 +0000

    Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,17 +6,25 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
+
+const queryString = `
 select distinct teachers.name as teacher, cohorts.name as cohort
 from assistance_requests 
 join teachers on (assistance_requests.teacher_id = teachers.id)
 join students on (assistance_requests.student_id= students.id)
 join cohorts on (cohorts.id= students.cohort_id)
-where cohorts.name LIKE '%${process.argv[2]}%' 
+where cohorts.name LIKE $1 
 order by teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+//const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`];
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`The teacher ${user.teacher} assisted the cohort ${user.cohort}`);
   })
 });
+
+
